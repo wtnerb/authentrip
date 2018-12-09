1992be85ddfd09575fea8059b621a1527e7b285d
wtnerb <wtnerb@gmail.com>
2018-12-08 19:06:12 -0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# authentrip-api
+This API is a backend service for the authentrip mobile application. This api will query for tipping ettiquette and exchange rates for the location where the customer resides and will query for nearby ATMs that service Discover cards.
+
+This app was produced as part of the hackyourtrip hackathon, sponsored by discover. They deserve much of the credit for making this possible.
+
+## Use
+To use this app, hit one of our endpoints. We only have a few, and they are tailored to the front end app.
+
+## Contributing
+Unless you can argue that the your PR will benefit the front end application, you pull request will be denied. Feel free to fork the app amd make the changes yourself!
+
+## Sources
+Thanks once again to Discover for gathering the authentrip app development team. We use their APIs for our service.
---
+++ authentrip/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace country_info.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        // GET api/values
+        [HttpGet("{Country}")]
+        public Country Get(string iso)
+        {
+            return new Country();
+        }
+
+
+        [HttpGet("{lat}/{long}")]
+        public IEnumerable<Atm> Get(double lat, double lon)
+        {
+            return new List<Atm> {
+                new Atm( "US bank ATM", 50.40001, 50.2222),
+                new Atm( "Bank of America ATM", 49.40001, 10.2222),
+                new Atm( "Chase Bank", 25.40001, 5.2222)
+            };
+        }
+
+    }
+
+    public struct Atm {
+        public Atm (string name, double latitude, double longitude){
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+        public string Name;
+        public double Latitude;
+        public double Longitude;
+    }
+
+    public struct Country {
+        public Country (string iso)
+    {
+        Exchange = 1.33215;
+        TipAmount = new Dictionary<string, int> () {
+            {"dining", 15 },
+            {"porter", 10}
+        };
+    }
+        public double Exchange;
+        public Dictionary<string, int> TipAmount;
+
+    }
+}
